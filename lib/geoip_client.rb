4f3acbb5875ff5a879d06161a465122cb1d918d5
Utkarsh Kukreti <utkarshkukreti@gmail.com>
2012-04-02 11:53:47 +0530

    Don't blow up on 127.0.0.1.

--- lib/geoip_client.rb
+++ lib/geoip_client.rb
@@ -11,7 +11,22 @@ module GeoIPClient
   end
 
   def self.look_up(ip)
-    json = JSON.parse(Net::HTTP.get(URI.parse("#{@host}/json/#{ip}")))
+    raw = Net::HTTP.get(URI.parse("#{@host}/json/#{ip}"))
+    if raw == 'null'
+      json = {
+        country_code: "LO",
+        country_code3: "LOC",
+        country_name: "Localhost Country",
+        region: "Localhost Region",
+        city: "Localhost City",
+        latitude: 0,
+        longitude: 0,
+        dma_code: 0,
+        area_code: 0
+      }
+    else
+      json = JSON.parse(raw)
+    end
     json.inject({}) do |hash, (key, value)|
       hash[key.to_sym] = value
       hash
